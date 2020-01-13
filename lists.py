3e37883f70239e4a228f2340f6ad8bb7bc4695e4
Ken Boyd <klb417@gmail.com>
2020-01-13 15:51:15 -0600

    slimmed code down

--- lists.py
+++ lists.py
@@ -13,15 +13,9 @@ numbers_1_to_10 = range(1, 11)
 
 # Iterate from 1 to 10
 for number in numbers_1_to_10:
-    the_numbers_match = False
 
     # Iterate your random number list here
-    for random_number in my_randoms:
-        if random_number == number:
-            # Does my_randoms contain number? Change the boolean.
-            the_numbers_match = True
-
-    if the_numbers_match:
+    if number in my_randoms:
         print(f'{number} is in the random list')
     else:
         print(f'{number} is not in the random list')
